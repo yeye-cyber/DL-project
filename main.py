b776313d62ee0bc3bbde500ee7397428f0ce0da5
yeye-cyber <75787758+yeye-cyber@users.noreply.github.com>
2022-05-16 10:34:07 +0200

    ihjui

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ from keras.preprocessing.image import ImageDataGenerator
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
+Ini = 'he_uniform'
+#Ini = tf.keras.initializers.RandomNormal(mean = 0.0, stddev=1.0)
+
 def see_data():
     (trainX, trainy), (testX, testy) = cifar10.load_data()
     print('Train: X=%s, y=%s' % (trainX.shape, trainy.shape))
@@ -51,30 +54,33 @@ def summerize_diagonstics(history):
     plt.plot(history.history['accuracy'], color ='blue', label ='train')
     plt.plot(history.history['val_accuracy'], color = 'orange', label = 'test')
     plt.legend()
-    filename = "Baseline 3 + batnorm aug drop"
+    filename = "Baseline 3 + batnorm aug drop mean 0 sig 1"
     plt.savefig(filename + '_plot.png')
     plt.close()
 
-def define_model(funlist):
+def define_model(funlist, adam):
     model = tf.keras.Sequential()
     for i in funlist:
         model.add(i)
     #compile
-    opt = tf.keras.optimizers.SGD(learning_rate = 0.01, momentum = 0.9)
+    if adam:
+        opt = tf.keras.optimizers.Adam(learning_rate = 0.01)
+    else:
+        opt = tf.keras.optimizers.SGD(learning_rate = 0.01, momentum = 0.9)
     model.compile(optimizer = opt, loss = 'categorical_crossentropy', metrics = ['accuracy'])
     return model
 
 def conv2(i, L2= False, input=False):
     if input:
         if L2:
-            return tf.keras.layers.Conv2D(32*i, (3,3), activation='relu', kernel_initializer='he_uniform', padding = 'same', kernel_regularizer=l2(0.001), input_shape=(32,32,3))
+            return tf.keras.layers.Conv2D(32*i, (3,3), activation='relu', kernel_initializer=Ini, padding = 'same', kernel_regularizer=l2(0.001), input_shape=(32,32,3))
         else:
-            return tf.keras.layers.Conv2D(32*i, (3,3), activation='relu', kernel_initializer='he_uniform', padding = 'same', input_shape=(32,32,3))
+            return tf.keras.layers.Conv2D(32*i, (3,3), activation='relu', kernel_initializer=Ini, padding = 'same', input_shape=(32,32,3))
     else:
         if L2:
-            return tf.keras.layers.Conv2D(32*i, (3,3), activation='relu', kernel_initializer='he_uniform', padding = 'same', kernel_regularizer=l2(0.001))
+            return tf.keras.layers.Conv2D(32*i, (3,3), activation='relu', kernel_initializer=Ini, padding = 'same', kernel_regularizer=l2(0.001))
         else:
-            return tf.keras.layers.Conv2D(32*i, (3,3), activation='relu', kernel_initializer='he_uniform', padding = 'same')
+            return tf.keras.layers.Conv2D(32*i, (3,3), activation='relu', kernel_initializer=Ini, padding = 'same')
 
 def max_pool():
     return tf.keras.layers.MaxPooling2D((2,2))
@@ -85,9 +91,9 @@ def flat():
 def dense(act, ini, i, L2=False):
     if ini:
         if L2:
-            return tf.keras.layers.Dense(i, activation=act, kernel_initializer='he_uniform', kernel_regularizer=l2(0.001))
+            return tf.keras.layers.Dense(i, activation=act, kernel_initializer=Ini, kernel_regularizer=l2(0.001))
         else:
-            return tf.keras.layers.Dense(i, activation=act, kernel_initializer='he_uniform')
+            return tf.keras.layers.Dense(i, activation=act, kernel_initializer=Ini)
     else:
         return tf.keras.layers.Dense(i, activation=act)
 def drop(i=0.2):
@@ -107,9 +113,13 @@ def batnorm():
 
 
 def main():
+    #TODO: Ändra ordning på dropout och batnorm (izi)
+    # learning rate schedulers (lite svårare)
+
     print(tf.test.is_gpu_available())
     L2=False #Weightdecay
     aug=True #Dataaugmentation
+    adam = False #adam optimizer
     #load data
     trainX, trainY, testX, testY = load_data()
     print('Train: X=%s, y=%s' % (trainX.shape, trainY.shape))
@@ -118,7 +128,7 @@ def main():
     trainX, testX = prep_pixels(trainX, testX)
     #define model
     funlist = [conv2(1, L2, input = True), batnorm(), conv2(1, L2), batnorm(), max_pool(), drop(0.2), conv2(2, L2), batnorm(), conv2(2, L2),batnorm(), max_pool(), drop(0.3), conv2(4, L2), batnorm(), conv2(4, L2), batnorm(), max_pool(), drop(0.4), flat(), dense('relu', True, 128, L2), batnorm(), drop(0.5), dense('softmax', False, 10, L2)]
-    model = define_model(funlist)
+    model = define_model(funlist, adam)
     #fit model
     history = augmentation(trainX, trainY, testX, testY, model, aug)
     #evaluate model
